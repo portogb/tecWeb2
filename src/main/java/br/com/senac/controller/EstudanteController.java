4c3040f66fdb2da9cd5be949213cabe08312533a
SENACRJEDU\36124872021.1N <porto.g4briel@gmail.com>
2023-03-14 21:14:32 -0300

    Adicionando novas funcionalidades

--- src/main/java/br/com/senac/controller/EstudanteController.java
+++ src/main/java/br/com/senac/controller/EstudanteController.java
@@ -1,9 +1,15 @@
 package br.com.senac.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +24,28 @@ public class EstudanteController {
 	private EstudanteService estudanteService;
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<Estudante> buscarEstudantePorId(@PathVariable(name = "id") Long id){
+	public ResponseEntity<Estudante> buscarEstudantePorId(@PathVariable Long id){
 		return estudanteService.buscarEstudantePorId(id);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Estudante>> buscarTodosEstudantes(){
+		return estudanteService.buscarTodosEstudantes();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Estudante> cadastrarEstudante(@RequestBody Estudante estudante){
+		return estudanteService.cadastrarEstudante(estudante);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Estudante> atualizarEstudante(@RequestBody Estudante estudante){
+		return estudanteService.atualizarEstudante(estudante);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deletarEstudante(@PathVariable Long id){
+		return estudanteService.removerUsuario(id);
 	}
 	
 }
