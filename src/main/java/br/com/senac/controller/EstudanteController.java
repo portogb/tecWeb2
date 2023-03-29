dba1425a1ee9aef32f0f025956beea3def04a237
SENACRJEDU\36124872021.1N <porto.g4briel@gmail.com>
2023-03-28 21:36:50 -0300

    Adicionado paginação

--- src/main/java/br/com/senac/controller/EstudanteController.java
+++ src/main/java/br/com/senac/controller/EstudanteController.java
@@ -3,6 +3,9 @@ package br.com.senac.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.senac.domain.Estudante;
@@ -46,6 +50,13 @@ public class EstudanteController {
 	@DeleteMapping("/{id}")
 	public ResponseEntity<String> removerEstudante(@PathVariable Long id){
 		return estudanteService.removerEstudante(id);
+	}
+	
+	@GetMapping("paginacao")
+	public Page<Estudante> buscarEstudantePorPaginacao(@RequestParam(defaultValue = "0") Integer pagina,
+			@RequestParam(defaultValue = "5") Integer itensPorPagina, @RequestParam(defaultValue = "0") String ord){
+//		return estudanteService.buscaEstudantePorPaginacao(PageRequest.of(pagina, itensPorPagina, Sort.by("nome").ascending()));
+		return estudanteService.buscaEstudantePorPaginacao(PageRequest.of(pagina, itensPorPagina, Sort.by(ord)));
 	}
 
 }
--- src/main/java/br/com/senac/service/EstudanteService.java
+++ src/main/java/br/com/senac/service/EstudanteService.java
@@ -5,60 +5,63 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.senac.domain.Estudante;
+import br.com.senac.repository.EstudanteRepository;
+import lombok.AllArgsConstructor;
 
 @Service
+@AllArgsConstructor
 public class EstudanteService {
 
-	private static Map<Long, Estudante> listaEstudantes = new HashMap<>();
+	private EstudanteRepository estudanteRepository;
 
 	public ResponseEntity<Estudante> buscarEstudantePorId(Long id) {
-		Estudante estudante = listaEstudantes.get(id);
-		if (estudante == null) {
-			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+		if (estudanteRepository.existsById(id)) {
+			return ResponseEntity.status(HttpStatus.OK).body(estudanteRepository.findById(id).get());
+		}
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+	}
+
+	public ResponseEntity<List<Estudante>> buscarTodosEstudantes() {
+		List listaEstudantes = estudanteRepository.findAll();
+
+		if (listaEstudantes != null) {
+			return ResponseEntity.status(HttpStatus.OK).body(listaEstudantes);
 		}
 
-		return ResponseEntity.status(HttpStatus.OK).body(estudante);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
 	}
-	
-	public ResponseEntity<List<Estudante>> buscarTodosEstudantes(){
-		List listaEstudante = new ArrayList<>(listaEstudantes.values());
-		if(listaEstudante.isEmpty()) {
-			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+
+	public ResponseEntity<Estudante> cadastrarEstudante(Estudante estudante) {
+		Estudante est = estudanteRepository.save(estudante);
+		return ResponseEntity.status(HttpStatus.CREATED).body(est);
+	}
+
+	public ResponseEntity<Estudante> atualizarEstudante(Long id, Estudante estudante) {
+		if (estudanteRepository.existsById(id)) {
+			Estudante est = estudanteRepository.save(estudante);
+			return ResponseEntity.status(HttpStatus.OK).body(est);
 		}
-		
-		return ResponseEntity.status(HttpStatus.FOUND).body(listaEstudante);
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 	}
-	
-	public ResponseEntity<Estudante> cadastrarEstudante(Estudante estudante){
-		listaEstudantes.put(estudante.getId(), estudante);
-		return ResponseEntity.status(HttpStatus.CREATED).body(estudante);
-	}
-	
-	public ResponseEntity<Estudante> atualizarEstudante(Long id, Estudante estudante){
-		Estudante estudanteEncontrado = listaEstudantes.get(id);
-		if(estudanteEncontrado == null) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+
+	public ResponseEntity<String> removerEstudante(Long id) {
+		if (estudanteRepository.existsById(id)) {
+			estudanteRepository.deleteById(id);
+			return ResponseEntity.status(HttpStatus.OK).body("Removido com louvor!");
 		}
-			
-		listaEstudantes.put(estudante.getId(), estudante);
-		
-		return ResponseEntity.status(HttpStatus.OK).body(estudante);
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("NULL");
 	}
 	
-	public ResponseEntity<String> removerEstudante(Long id){
-		Estudante estudanteEncontrado = listaEstudantes.get(id);
-		
-		if(estudanteEncontrado == null) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-		}
-		
-		listaEstudantes.remove(id);
-		return ResponseEntity.status(HttpStatus.OK).body("Removido com louvor!");
+	public Page<Estudante> buscaEstudantePorPaginacao(PageRequest page){
+		return estudanteRepository.findAll(page);
 	}
-	
 }
